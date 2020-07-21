439f2f895283d9e24bfec9c23d8d376fb7053918
corbennett <corbennett@gmail.com>
2020-07-21 13:04:08 -0700

    make launch more user friendly

--- launch.bat
+++ launch.bat
@@ -1,3 +1,9 @@
+ECHO off
+title Script to get RFs given data directory
+ECHO activating environment: np_pipeline_validation_clone
 call conda activate np_pipeline_validation_clone
-call cd C:\Users\svc_neuropix\Documents\GitHub\NP_pipeline_validation\experiment qc
+ECHO navigating to code directory
+call cd C:\Users\svc_neuropix\Documents\GitHub\NP_pipeline_QC
+set /p data_dir=Base data directory: 
+call python get_RFs_standalone.py %data_dir%
 cmd \k
