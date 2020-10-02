763e773590aa4b1195d14443ceaa63921e2f370d
taminar <taminar@alleninstitute.org>
2020-10-02 14:29:36 -0700

    numpy version compat for sweep_order

--- launch.bat
+++ launch.bat
@@ -1,9 +1,10 @@
 ECHO off
 title Script to get RFs given data directory
-ECHO activating environment: np_pipeline_validation_clone
-call conda activate np_pipeline_validation_clone
+ECHO activating environment: qc
+call C:\ProgramData\Anaconda3\Scripts\activate.bat C:\ProgramData\Anaconda3
+call conda activate qc
 ECHO navigating to code directory
-call cd C:\Users\svc_neuropix\Documents\GitHub\NP_pipeline_QC
+call cd C:\Users\taminar\Documents\GitHub\NP_pipeline_QC
 set /p data_dir=Base data directory: 
 call python get_RFs_standalone.py %data_dir%
 cmd \k
